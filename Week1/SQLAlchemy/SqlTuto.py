5cb8c7e91bca191a31802cbe7129bc6a81d7f937
FernandoPereiraM <103136298+FernandoPereiraM@users.noreply.github.com>
2024-02-11 11:41:07 -0500

    Sql

--- Week1/SQLAlchemy/SqlTuto.py
+++ Week1/SQLAlchemy/SqlTuto.py
@@ -2,8 +2,9 @@ import sqlalchemy as db
 
 engine = db.create_engine("sqlite:///european_database.sqlite")
 
-conn = engine.connect() 
+conn = engine.connect()
 
-metadata = db.MetaData() #extracting the metadata
-division= db.Table('divisions', metadata, autoload=True, 
-autoload_with=engine) #Table object
+metadata = db.MetaData()  # extracting the metadata
+division = db.Table('divisions', metadata, autoload_with=engine)  # Table object
+
+print(repr(metadata.tables['divisions']))
